77a7876e4a25e56a28e560d721c89e97d7808f48
neil <msg@nancode.cn>
2022-11-03 19:28:35 +0800

    feat: update submit your project url
    Signed-off-by: neil <msg@nancode.cn>

--- src/components/Misc/SubmitYouProject.tsx
+++ src/components/Misc/SubmitYouProject.tsx
@@ -4,14 +4,14 @@ import classnames from 'classnames';
 
 const SubmitYouProject: React.FC<{ blackMode?: boolean }> = ({ blackMode }) => {
   return (
-    <Link
-      href="/submit-your-project"
+    <a
+      href="https://compass.gitee.com/submit-your-project"
       className={
         'cursor-pointer border-2 border-solid border-white bg-black px-6 py-3 font-semibold text-white'
       }
     >
       Submit your project
-    </Link>
+    </a>
   );
 };
 
