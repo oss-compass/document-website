4a9d3904293edb61f6f77cf38f09103bea50e347
neil <msg@nancode.cn>
2022-12-08 14:25:46 +0800

    feat: submodule url update
    Signed-off-by: neil <msg@nancode.cn>

--- copy.sh
+++ copy.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-rsync -a -v --ignore-existing modules/ i18n/en/docusaurus-plugin-content-docs/current/
-rsync -a -v --ignore-existing modules/ i18n/zh/docusaurus-plugin-content-docs/current/
+# en
+mkdir -p i18n/en/docusaurus-plugin-content-docs/current
+rsync -a -v --ignore-existing modules/docs/ i18n/en/docusaurus-plugin-content-docs/current/docs/
+rsync -a -v --ignore-existing modules/community/ i18n/en/docusaurus-plugin-content-docs/current/community/
+
+# zh
+mkdir -p i18n/zh/docusaurus-plugin-content-docs/current
+rsync -a -v modules/docs-zh/ i18n/zh/docusaurus-plugin-content-docs/current/docs/
