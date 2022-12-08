5930a6cefe50d24da438a7170f255743c550297a
neil <msg@nancode.cn>
2022-12-08 15:58:52 +0800

    feat: add community-zh in repo
    Signed-off-by: neil <msg@nancode.cn>

--- copy.sh
+++ copy.sh
@@ -2,9 +2,21 @@
 
 # en
 mkdir -p i18n/en/docusaurus-plugin-content-docs/current
+# en doc
 rsync -a -v --ignore-existing modules/docs/ i18n/en/docusaurus-plugin-content-docs/current/docs/
+# en community
 rsync -a -v --ignore-existing modules/community/ i18n/en/docusaurus-plugin-content-docs/current/community/
+
+
+
+
 
 # zh
 mkdir -p i18n/zh/docusaurus-plugin-content-docs/current
-rsync -a -v modules/docs-zh/ i18n/zh/docusaurus-plugin-content-docs/current/docs/
+# zh docs
+rsync -a -v localization_repos/docs-zh/ i18n/zh/docusaurus-plugin-content-docs/current/docs/
+# zh community
+rsync -a -v localization_repos/community-zh/ i18n/zh/docusaurus-plugin-content-docs/current/community/
+
+# sync images
+rsync -a -v --ignore-existing modules/community/ i18n/zh/docusaurus-plugin-content-docs/current/community/
