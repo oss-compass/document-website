47e22b5ad89b32fee10377eb961122af02fb20a0
neil <msg@nancode.cn>
2023-01-10 16:29:36 +0800

    chore: change submodule folder
    Signed-off-by: neil <msg@nancode.cn>

--- copy.sh
+++ copy.sh
@@ -14,9 +14,9 @@ rsync -a -v --ignore-existing modules/community/ i18n/en/docusaurus-plugin-conte
 # zh
 mkdir -p i18n/zh/docusaurus-plugin-content-docs/current
 # zh docs
-rsync -a -v localization_repos/docs-zh/ i18n/zh/docusaurus-plugin-content-docs/current/docs/
+rsync -a -v modules_i18n/docs-zh/ i18n/zh/docusaurus-plugin-content-docs/current/docs/
 # zh community
-rsync -a -v localization_repos/community-zh/ i18n/zh/docusaurus-plugin-content-docs/current/community/
+rsync -a -v modules_i18n/community-zh/ i18n/zh/docusaurus-plugin-content-docs/current/community/
 
 # sync images
 rsync -a -v --ignore-existing modules/community/ i18n/zh/docusaurus-plugin-content-docs/current/community/
