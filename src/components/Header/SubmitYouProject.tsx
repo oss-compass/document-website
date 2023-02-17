a802c7983d24dcac99f012b90d70c9945662f0c5
neil <msg@nancode.cn>
2023-02-17 17:48:14 +0800

    style: button padding
    Signed-off-by: neil <msg@nancode.cn>

--- src/components/Header/SubmitYouProject.tsx
+++ src/components/Header/SubmitYouProject.tsx
@@ -6,7 +6,7 @@ const SubmitYouProject: React.FC<{ blackMode?: boolean }> = ({ blackMode }) => {
     <a
       href="/submit-your-project"
       className={
-        'ml-5 shrink-0 cursor-pointer truncate border-2  border-solid border-white bg-black px-6 py-3 font-medium text-white'
+        'ml-5 shrink-0 cursor-pointer truncate border-2  border-solid border-white bg-black px-6 py-2 font-medium text-white'
       }
     >
       <Translate id={'header.submit_your_project'} />
