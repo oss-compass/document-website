1cfd7903e49bd36ef4764147542b35aaa0133a34
laixingyou <laixy7@gmail.com>
2023-06-02 10:30:40 +0800

    feat: add pdf viewer component (#11)
    * feat: add pdf viewer component
    
    Signed-off-by: laixingyou <laixy7@gmail.com>
    
    * fix:  optimize pdf width
    
    Signed-off-by: laixingyou <laixy7@gmail.com>
    
    ---------
    
    Signed-off-by: laixingyou <laixy7@gmail.com>

---
+++ src/components/PdfViewer.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
+import 'react-pdf/dist/esm/Page/TextLayer.css';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import '../style/pdfviewer.scss';
+
+const PdfViewer = ({ pdfUrl }) => {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const [width, setWidth] = useState(0);
+
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+    setWidth(document.getElementById('ResumeContainer').offsetWidth);
+  };
+
+  return (
+    <div id="ResumeContainer" className="my-4">
+      <Document
+        className={'PDFDocument'}
+        file={pdfUrl}
+        onLoadSuccess={onDocumentLoadSuccess}
+        renderMode="canvas"
+      >
+        {Array.from(new Array(numPages), (el, index) => (
+          <Page
+            width={width}
+            key={`page_${index + 1}`}
+            pageNumber={index + 1}
+            className={'PDFPage PDFPageOne'}
+            renderTextLayer={false}
+          />
+        ))}
+        {/* <Page
+          pageNumber={pageNumber}
+          className={'PDFPage PDFPageOne'}
+          renderTextLayer={false}
+        /> */}
+      </Document>
+      {/* <p>
+        Page {pageNumber}of {numPages}
+      </p>
+      <button
+        disabled={pageNumber <= 1}
+        onClick={() => setPageNumber(pageNumber - 1)}
+      >
+        Prev
+      </button>
+      <button
+        disabled={pageNumber >= numPages}
+        onClick={() => setPageNumber(pageNumber + 1)}
+      >
+        Next
+      </button> */}
+    </div>
+  );
+};
+
+export default PdfViewer;
