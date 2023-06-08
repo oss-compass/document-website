7e2ffb8e5ac5906f36d71378a27a4d076a4e8e12
neil <msg@nancode.cn>
2023-06-08 17:03:43 +0800

    chore: rsync add delete args
    Signed-off-by: neil <msg@nancode.cn>

--- copy.sh
+++ copy.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
 
 mkdir -p i18n/en/docusaurus-plugin-content-docs/current
+mkdir -p modules/community/blog
+mkdir -p modules_i18n/community-zh/blog
 
 # en doc
-rsync -a --exclude={'.git','.github'} modules/docs/ i18n/en/docusaurus-plugin-content-docs/current/docs/
+rsync -a --delete --exclude={'.git','.github'} modules/docs/ i18n/en/docusaurus-plugin-content-docs/current/docs/
 # en community
-rsync -a --exclude={'.git','.github'} modules/community/ i18n/en/docusaurus-plugin-content-docs/current/community/
+rsync -a --delete --exclude={'.git','.github'} modules/community/ i18n/en/docusaurus-plugin-content-docs/current/community/
 # en blog
-rsync -a --exclude={'.git','.github'} modules/community/blog/ blog/
+rsync -a --delete --exclude={'.git','.github'} modules/community/blog/ blog/
 
 
 
 # zh
 mkdir -p i18n/zh/docusaurus-plugin-content-docs/current
 # zh docs
-rsync -a --exclude={'.git','.github'} modules_i18n/docs-zh/ i18n/zh/docusaurus-plugin-content-docs/current/docs/
+rsync -a --delete --exclude={'.git','.github'} modules_i18n/docs-zh/ i18n/zh/docusaurus-plugin-content-docs/current/docs/
 # zh community
-rsync -a --exclude={'.git','.github'} modules_i18n/community-zh/ i18n/zh/docusaurus-plugin-content-docs/current/community/
+rsync -a --delete --exclude={'.git','.github'} modules_i18n/community-zh/ i18n/zh/docusaurus-plugin-content-docs/current/community/
 # zh blog
-rsync -a --exclude={'.git','.github'} modules_i18n/community-zh/blog/ i18n/zh/docusaurus-plugin-content-blog/
+rsync -a --delete --exclude={'.git','.github','options.json'} modules_i18n/community-zh/blog/ i18n/zh/docusaurus-plugin-content-blog/
 
