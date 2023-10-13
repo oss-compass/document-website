dce8a0257ea15a5164f9b7d622cb9982f7e09519
laixingyou <laixy7@gmail.com>
2023-10-13 16:50:38 +0800

    fix: copy shell
    Signed-off-by: laixingyou <laixy7@gmail.com>

--- copy.sh
+++ copy.sh
@@ -20,5 +20,5 @@ rsync -a --delete --exclude={'.github'} modules_i18n/docs-zh/ i18n/zh/docusaurus
 # zh community
 rsync -a --delete --exclude={'.github'} modules_i18n/community-zh/ i18n/zh/docusaurus-plugin-content-docs/current/community/
 # zh blog
-rsync -a --delete --exclude={'.github','options.json'} modules_i18n/community-zh/blog/ i18n/zh/docusaurus-plugin-content-blog/
+rsync -a --exclude={'.github'} modules_i18n/community-zh/blog/ i18n/zh/docusaurus-plugin-content-blog/
 
